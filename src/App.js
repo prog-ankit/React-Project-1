84f9a1a04cd93610c9f12e2337701141a3a01527
user_name <21itank509@ldce.ac.in>
2022-07-28 11:18:11 +0530

    Files Changed till adding to Github

--- src/App.js
+++ src/App.js
@@ -1,18 +1,69 @@
 import './App.css';
+import React, { useState } from 'react'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+
+} from "react-router-dom";
+// Next Task is to Add Different Color Modes
+// Solve the bug word count bug
+
+import About from './components/About';
 import NavBar from './components/NavBar';
 import TextArea from './components/TextArea';
+import Alert from './components/Alert';
 //Proptypes is used to give restrictions passed to props of functions
 function App() {
-  let msg = "Here is My message to pass here!!";
+
+  const [mode, setMode] = useState('light');
+  // const [green, setGreen] = useState('light');
+
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+
+    setAlert({
+      msg: message,
+      type: type
+    });
+
+    setTimeout(() => {
+      setAlert(null);
+    }, 3000);
+  }
+
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#010a5e';//grey  
+      showAlert('Dark Mode has been Enabled', 'success');
+
+    } else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert('Light Mode has been Enabled', 'success');
+    }
+  }
   return (
     <>
-      
-      {/* <NavBar page="My Site" title="Home" about={msg}/> */}
-      <NavBar page="My Site" title="Home"/>
-      <div className="container my-3">
-       <TextArea heading="Analyze and Compute your text"/>
-      </div>
-      
+      <BrowserRouter>
+
+        {/* <NavBar page="My Site" title="Home" about={msg}/> */}
+
+        <NavBar page="My Site" title="Home" about='About Us' mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route exact path='/' element={<TextArea heading="Analyze and Compute your text" mode={mode} showalert={showAlert}/>}></Route>
+            <Route exact path="/about" element={<About mode={mode}/>}></Route>
+          </Routes>
+
+        </div>
+
+
+      </BrowserRouter>
     </>
   );
 }
--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -1,39 +1,73 @@
 import React, { useState } from 'react'
 export default function TextArea(props) {
+    let name = 'Ankit Bose';
     const [text, setText] = useState("");
     const ChangeText = (event) => {
         //console.log("Text Changed");
+
         setText(event.target.value);
     }
     const UpperClick = () => {
         //console.log("Button Clicked");
         let newText = text.toUpperCase();
         setText(newText);
+        props.showalert('Converted to Uppercase', 'success');
     }
     const LowerClick = () => {
         //console.log("Button Clicked");
         let newText = text.toLowerCase();
         setText(newText);
+        props.showalert('Converted to LowerCase', 'info');
+    }
+    const copyText = () => {
+        //console.log("Button Clicked");
+        var box = document.getElementById('myarea');
+        box.select();
+        navigator.clipboard.writeText(box.value);
+        props.showalert('Text Copied', 'danger');
+    }
+    const clearBox = () => {
+        //console.log("Button Clicked");
+        setText('');
+        props.showalert('Box Cleared.', 'success');
     }
     return (
         <>
-            <div className='container'>
-                <h1>{props.heading}</h1>
+            {/* <div className="container d-flex">
+                <button type="submit" className="btn btn-primary mx-1">Hello</button>
+
+                <button type="submit" className="btn btn-primary mx-2">Hello</button>
+
+                <button type="submit" className="btn btn-primary mx-2">Hello</button>
+
+                <button type="submit" className="btn btn-primary mx-2">Hello</button>
+
+                <button type="submit" className="btn btn-primary mx-2">Hello</button>
+
+                <input class="form-control" style={{width: "20%"}} type="search" placeholder="Search" aria-label="Search"/>
+                    
+
+            </div> */}
+            <div className='container' style={{color : props.mode==='light'?'#010a5e':'white'}}>
+                <h1 style={{color : props.mode==='light'?'#010a5e':'white'}}>{props.heading}</h1>
                 <div className="mb-3">
-                    <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-                    <textarea className="form-control" id="exampleFormControlTextarea1" onChange={ChangeText} rows="8" value={text}></textarea>
+                    <label style={{color : props.mode==='light'?'#010a5e':'white'}} htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+                    <textarea className="form-control" id="myarea" style={{color : props.mode==='light'?'#010a5e':'white', backgroundColor : props.mode==='dark'?'#010a5e':'white'}} onChange={ChangeText} rows="8" value={text}></textarea>
                 </div>
                 <button type="submit" className="btn btn-primary mx-1" onClick={UpperClick}>Convert to UpperCase</button>
                 <button type="submit" className="btn btn-primary mx-2" onClick={LowerClick}>Convert to LowerCase</button>
+                <button type="submit" className="btn btn-primary mx-2" onClick={copyText}>Copy Text</button>
+                <button type="submit" className="btn btn-primary mx-2" onClick={clearBox}>Clear Text</button>
+                
             </div>
-            <div className="container my-2">
+            <div className="container my-2" style={{color : props.mode==='light'?'#010a5e':'white'}}>
                 <h2>Your Text Summary</h2>
                 <p>{text.split(" ").length} words and {text.length} characters</p>
                 <p>{0.08 * text.split(" ").length} Minuted to Read</p>
             </div>
-            <div className="container my-2">
+            <div className="container my-2" style={{color : props.mode==='light'?'#010a5e':'white'}}>
                 <h2>Preview</h2>
-                <p>{text}</p>
+                <p>{text.length>0?text:`Enter your text iin textbox to preview here ${name}!!` }</p>
             </div>
         </>
     )
